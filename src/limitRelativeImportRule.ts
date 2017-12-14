b944fdfb65e7fc431e1a11a78c1bfdf8aa0a8b37
jonathanmourtada <jonathan.mourtada@divid.se>
2017-12-14 14:09:28 +0100

    wip: limit relative import

--- src/limitRelativeImportRule.ts
+++ src/limitRelativeImportRule.ts
@@ -1,7 +1,11 @@
 import * as ts from "typescript";
 import * as Lint from "tslint";
+import * as path from "path";
 
 export class Rule extends Lint.Rules.AbstractRule {
+  constructor(options: Lint.IOptions) {
+    super(options);
+  }
   public static FAILURE_STRING = "Import violating limit-relative-import rule";
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
@@ -13,15 +17,45 @@ export class Rule extends Lint.Rules.AbstractRule {
 
 // The walker takes care of all the work.
 class NoImportsWalker extends Lint.RuleWalker {
+  private containment: string = "./";
+  constructor(sourceFile: ts.SourceFile, options: Lint.IOptions) {
+    super(sourceFile, options);
+    const containmentRelative = options.ruleArguments[0] as string | undefined;
+    if (!containmentRelative) {
+      throw new Error("Missing relative containment path");
+    }
+
+    this.containment = path.resolve(process.cwd(), containmentRelative);
+  }
   public visitImportDeclaration(node: ts.ImportDeclaration): void {
-    console.log(node.moduleSpecifier.getText());
-    this.addFailureAtNode(node.moduleSpecifier, Rule.FAILURE_STRING);
-    // create a failure at the current position
-    /* this.addFailure(
-      this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING)
-    ); */
+    const sourceFileName = node.getSourceFile().fileName;
+    const importText = node.moduleSpecifier
+      .getText()
+      .replace(/^"(.+?)"$/, "$1");
+
+    if (
+      !importText.startsWith("../") ||
+      !isInside(this.containment, sourceFileName)
+    ) {
+      super.visitImportDeclaration(node);
+      return;
+    }
+
+    const stepsUp = importText.split("/").filter(s => s === "..");
+    const resultParent = path.resolve(
+      this.containment,
+      ...stepsUp.map(s => `${s}/`)
+    );
+
+    if (resultParent.length < this.containment.length) {
+      this.addFailureAtNode(node.moduleSpecifier, Rule.FAILURE_STRING);
+    }
 
     // call the base version of this visitor to actually parse this node
     super.visitImportDeclaration(node);
   }
+}
+
+function isInside(containment: string, fileName: string): boolean {
+  return fileName.replace(containment, "").length < fileName.length;
 }
