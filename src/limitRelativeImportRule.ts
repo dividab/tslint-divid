41355b59b0f18a779c67f1fcf536d7f45061ccb0
jonathanmourtada <jonathan.mourtada@divid.se>
2017-12-15 10:06:36 +0100

    Fix relative resolving bags. And simpler resolution logic

--- src/limitRelativeImportRule.ts
+++ src/limitRelativeImportRule.ts
@@ -15,13 +15,30 @@ export class Rule extends Lint.Rules.AbstractRule {
   public static FAILURE_STRING = "Import violating limit-relative-import rule";
 
   public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+    const sourceFileName = sourceFile.fileName;
+    const absoulutFileName = path.isAbsolute(sourceFileName)
+      ? sourceFileName
+      : path.resolve(sourceFileName);
+
+    const sourceFileDirectory = getDirectoryNameFromFullFileNamePath(
+      absoulutFileName
+    );
+
+    if (!isInside(this.containment, sourceFileDirectory)) {
+      return [];
+    }
+
+    const maxLevels =
+      path.relative(sourceFileDirectory, this.containment).split("..").length -
+      1;
+
     return this.applyWithFunction(sourceFile, (ctx: Lint.WalkContext<void>) => {
-      checkNode(ctx, this.containment);
+      checkNode(ctx, maxLevels);
     });
   }
 }
 
-function checkNode(ctx: Lint.WalkContext<void>, containment: string): void {
+function checkNode(ctx: Lint.WalkContext<void>, maxLevels: number): void {
   return ts.forEachChild(ctx.sourceFile, cb);
 
   function cb(node: ts.Node): void {
@@ -30,26 +47,17 @@ function checkNode(ctx: Lint.WalkContext<void>, containment: string): void {
     }
 
     const importDeclaration = node as ts.ImportDeclaration;
-
-    const sourceFileName = node.getSourceFile().fileName;
     const importText = importDeclaration.moduleSpecifier
       .getText()
       .replace(/^"(.+?)"$/, "$1");
 
-    if (
-      !importText.startsWith("../") ||
-      !isInside(containment, sourceFileName)
-    ) {
+    if (!importText.startsWith("../")) {
       return;
     }
 
-    const stepsUp = importText.split("/").filter(s => s === "..");
-    const resultParent = path.join(sourceFileName, ...stepsUp.map(s => s));
-    /* console.log("------------------------------------------------");
-    console.log("containment", containment);
-    console.log("sourceFileName", sourceFileName);
-    console.log("resultParent", resultParent); */
-    if (resultParent.length <= containment.length) {
+    const levels = countLevels(importText);
+    // console.log("levels", levels);
+    if (levels >= maxLevels) {
       ctx.addFailureAtNode(
         importDeclaration.moduleSpecifier,
         Rule.FAILURE_STRING
@@ -58,6 +66,23 @@ function checkNode(ctx: Lint.WalkContext<void>, containment: string): void {
   }
 }
 
+function getDirectoryNameFromFullFileNamePath(fileNamePath: string): string {
+  return fileNamePath.replace(/(.*?\/)[^\/]+?$/, "$1");
+}
+
 function isInside(containment: string, fileName: string): boolean {
   return fileName.replace(containment, "").length < fileName.length;
+}
+
+function countLevels(importText: string): number {
+  // console.log("importText", importText);
+  return importText.split("/").reduce((soFar: number, current: string) => {
+    // console.log("current", current);
+    switch (current) {
+      case "..":
+        return soFar + 1;
+      default:
+        return soFar;
+    }
+  }, 0);
 }
